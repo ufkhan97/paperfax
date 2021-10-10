c05b6a0b2a3441aa1d8628e1405331b8d5a637d0
Lawrence Wu <llwu@llwu.me>
2021-10-10 12:16:34 -0700

    brogress

--- src/components/AddAuditButton.tsx
+++ src/components/AddAuditButton.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 
 import NewAuditForm from 'components/NewAuditForm'
 
@@ -9,15 +9,31 @@ interface AddAuditButtonProps {
 const AddAuditButton: React.FC<AddAuditButtonProps> = ({ paperfaxId }) => {
   const [modalOpen, setModalOpen] = useState(false)
   const handleOpen = () => setModalOpen(true)
-  const handleClickClose = (e: React.MouseEvent<HTMLDivElement>) => {
+  const handleClickClose = (e: React.MouseEvent) => {
     setModalOpen(false)
     e.stopPropagation()
   }
 
+  const handleEsc = (event: KeyboardEvent) => {
+    if (event.key === 'Escape') {
+      setModalOpen(false)
+    }
+  }
+
+  useEffect(() => {
+    document.addEventListener('keydown', handleEsc, false)
+    return () => {
+      document.removeEventListener('keydown', handleEsc, false)
+    }
+  }, [])
+
   return (
     <>
       <div className={`modal-container ${modalOpen ? 'open' : ''}`} onClick={handleClickClose}>
-        <div className="modal" onClick={e => e.stopPropagation}>
+        <div className="modal" onClick={e => e.stopPropagation()}>
+          <a className="x-button" onClick={handleClickClose}>
+            ×
+          </a>
           <NewAuditForm paperfaxId={paperfaxId} />
         </div>
       </div>
