2586f0cef745f970ad0b355a6671fbbc660846b3
Lawrence Wu <llwu@llwu.me>
2021-10-10 07:48:59 -0700

    new verdict

--- src/components/AddAuditButton.tsx
+++ src/components/AddAuditButton.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { useWeb3ExecuteFunction } from 'react-moralis'
 
 import { CONTRACT_ADDRESS, CONTRACT_ABI } from 'rinkeby'
@@ -7,14 +7,23 @@ interface AddAuditButtonProps {
   paperfaxId: number
 }
 
+enum Verdict {
+  ENDORSED = 0,
+  NEEDS_REVIEW
+}
+
 const AddAuditButton: React.FC<AddAuditButtonProps> = ({ paperfaxId }) => {
+  const [verdict, setVerdict] = useState(Verdict.ENDORSED);
+  const handleVerdict = (e: any) => setVerdict(Number(e.target.value))
+  console.log(verdict)
+
   const { error, fetch, isFetching, isLoading } = useWeb3ExecuteFunction({
     abi: CONTRACT_ABI,
     contractAddress: CONTRACT_ADDRESS,
     functionName: 'createAudit',
     params: {
       _paperfaxId: paperfaxId,
-      _verdict: "0",
+      _verdict: `${verdict}`,
       _ipfsHash: "0",
       _numMinorIssues: "0",
       _numMajorIssues: "0",
@@ -31,6 +40,11 @@ const AddAuditButton: React.FC<AddAuditButtonProps> = ({ paperfaxId }) => {
           <pre>{JSON.stringify(error, null, 2)}</pre>
         </>
       )}
+      <div onChange={handleVerdict}>
+        <input type="radio" value="0" name="verdict" /> Green
+        <input type="radio" value="1" name="verdict" /> Yellow
+        <input type="radio" value="2" name="verdict" /> Red
+      </div>
       <button onClick={() => fetch()} disabled={isFetching}>
         Create New Audit
       </button>
--- src/rinkeby.ts
+++ src/rinkeby.ts
@@ -1,208 +1,208 @@
-export const CONTRACT_ADDRESS = '0x1f9c88C45f11A500332584A3fD42bD20596D71fD'
+export const CONTRACT_ADDRESS = '0x5A5177BC51b7907588Eb377852C739187bFF2b46'
 export const CONTRACT_ABI = [
   {
-    inputs: [
+    "inputs": [
       {
-        internalType: 'uint256',
-        name: '_paperfaxId',
-        type: 'uint256'
+        "internalType": "uint256",
+        "name": "_paperfaxId",
+        "type": "uint256"
       },
       {
-        internalType: 'enum PaperfaxIndex.Verdict',
-        name: '_verdict',
-        type: 'uint8'
+        "internalType": "enum PaperfaxIndex.Verdict",
+        "name": "_verdict",
+        "type": "uint8"
       },
       {
-        internalType: 'string',
-        name: '_ipfsHash',
-        type: 'string'
+        "internalType": "string",
+        "name": "_ipfsHash",
+        "type": "string"
       },
       {
-        internalType: 'uint32',
-        name: '_numMinorIssues',
-        type: 'uint32'
+        "internalType": "uint32",
+        "name": "_numMinorIssues",
+        "type": "uint32"
       },
       {
-        internalType: 'uint32',
-        name: '_numMajorIssues',
-        type: 'uint32'
+        "internalType": "uint32",
+        "name": "_numMajorIssues",
+        "type": "uint32"
       },
       {
-        internalType: 'uint32',
-        name: '_numRecommendations',
-        type: 'uint32'
+        "internalType": "uint32",
+        "name": "_numRecommendations",
+        "type": "uint32"
       }
     ],
-    name: 'createAudit',
-    outputs: [],
-    stateMutability: 'nonpayable',
-    type: 'function'
+    "name": "createAudit",
+    "outputs": [],
+    "stateMutability": "nonpayable",
+    "type": "function"
   },
   {
-    inputs: [
+    "inputs": [
       {
-        internalType: 'string[]',
-        name: '_paperURIs',
-        type: 'string[]'
+        "internalType": "string[]",
+        "name": "_paperURIs",
+        "type": "string[]"
       }
     ],
-    name: 'initializePaperfax',
-    outputs: [],
-    stateMutability: 'nonpayable',
-    type: 'function'
+    "name": "initializePaperfax",
+    "outputs": [],
+    "stateMutability": "nonpayable",
+    "type": "function"
   },
   {
-    inputs: [],
-    name: 'numAudits',
-    outputs: [
+    "inputs": [],
+    "name": "numAudits",
+    "outputs": [
       {
-        internalType: 'uint256',
-        name: '',
-        type: 'uint256'
+        "internalType": "uint256",
+        "name": "",
+        "type": "uint256"
       }
     ],
-    stateMutability: 'view',
-    type: 'function'
+    "stateMutability": "view",
+    "type": "function"
   },
   {
-    inputs: [],
-    name: 'numPaperfaxes',
-    outputs: [
+    "inputs": [],
+    "name": "numPaperfaxes",
+    "outputs": [
       {
-        internalType: 'uint256',
-        name: '',
-        type: 'uint256'
+        "internalType": "uint256",
+        "name": "",
+        "type": "uint256"
       }
     ],
-    stateMutability: 'view',
-    type: 'function'
+    "stateMutability": "view",
+    "type": "function"
   },
   {
-    inputs: [
+    "inputs": [
       {
-        internalType: 'string',
-        name: '_paperURI',
-        type: 'string'
+        "internalType": "string",
+        "name": "_paperURI",
+        "type": "string"
       }
     ],
-    name: 'paperfaxDetails',
-    outputs: [
+    "name": "paperfaxDetails",
+    "outputs": [
       {
-        internalType: 'uint256',
-        name: 'paperfaxId',
-        type: 'uint256'
+        "internalType": "uint256",
+        "name": "paperfaxId",
+        "type": "uint256"
       },
       {
-        internalType: 'enum PaperfaxIndex.Verdict[]',
-        name: 'verdicts',
-        type: 'uint8[]'
+        "internalType": "enum PaperfaxIndex.Verdict[]",
+        "name": "verdicts",
+        "type": "uint8[]"
       },
       {
-        internalType: 'string[]',
-        name: 'ipfsHashes',
-        type: 'string[]'
+        "internalType": "string[]",
+        "name": "ipfsHashes",
+        "type": "string[]"
       },
       {
-        internalType: 'uint32[]',
-        name: 'numMinorIssues',
-        type: 'uint32[]'
+        "internalType": "uint32[]",
+        "name": "numMinorIssues",
+        "type": "uint32[]"
       },
       {
-        internalType: 'uint32[]',
-        name: 'numMajorIssues',
-        type: 'uint32[]'
+        "internalType": "uint32[]",
+        "name": "numMajorIssues",
+        "type": "uint32[]"
       },
       {
-        internalType: 'uint32[]',
-        name: 'numRecommendations',
-        type: 'uint32[]'
+        "internalType": "uint32[]",
+        "name": "numRecommendations",
+        "type": "uint32[]"
       },
       {
-        internalType: 'uint32[]',
-        name: 'upvotes',
-        type: 'uint32[]'
+        "internalType": "uint32[]",
+        "name": "upvotes",
+        "type": "uint32[]"
       },
       {
-        internalType: 'uint32[]',
-        name: 'downvotes',
-        type: 'uint32[]'
+        "internalType": "uint32[]",
+        "name": "downvotes",
+        "type": "uint32[]"
       }
     ],
-    stateMutability: 'view',
-    type: 'function'
+    "stateMutability": "view",
+    "type": "function"
   },
   {
-    inputs: [
+    "inputs": [
       {
-        internalType: 'uint256',
-        name: '_paperfaxId',
-        type: 'uint256'
+        "internalType": "uint256",
+        "name": "_paperfaxId",
+        "type": "uint256"
       }
     ],
-    name: 'requestAudit',
-    outputs: [],
-    stateMutability: 'nonpayable',
-    type: 'function'
+    "name": "requestAudit",
+    "outputs": [],
+    "stateMutability": "nonpayable",
+    "type": "function"
   },
   {
-    inputs: [
+    "inputs": [
       {
-        internalType: 'uint256',
-        name: '_auditId',
-        type: 'uint256'
+        "internalType": "uint256",
+        "name": "_auditId",
+        "type": "uint256"
       },
       {
-        internalType: 'enum PaperfaxIndex.Vote',
-        name: '_type',
-        type: 'uint8'
+        "internalType": "enum PaperfaxIndex.Vote",
+        "name": "_type",
+        "type": "uint8"
       }
     ],
-    name: 'upvoteAudit',
-    outputs: [],
-    stateMutability: 'nonpayable',
-    type: 'function'
+    "name": "upvoteAudit",
+    "outputs": [],
+    "stateMutability": "nonpayable",
+    "type": "function"
   },
   {
-    inputs: [
+    "inputs": [
       {
-        internalType: 'string',
-        name: '',
-        type: 'string'
+        "internalType": "string",
+        "name": "",
+        "type": "string"
       }
     ],
-    name: 'uriToPaperfaxId',
-    outputs: [
+    "name": "uriToPaperfaxId",
+    "outputs": [
       {
-        internalType: 'uint256',
-        name: '',
-        type: 'uint256'
+        "internalType": "uint256",
+        "name": "",
+        "type": "uint256"
       }
     ],
-    stateMutability: 'view',
-    type: 'function'
+    "stateMutability": "view",
+    "type": "function"
   },
   {
-    inputs: [
+    "inputs": [
       {
-        internalType: 'address',
-        name: '',
-        type: 'address'
+        "internalType": "address",
+        "name": "",
+        "type": "address"
       },
       {
-        internalType: 'uint256',
-        name: '',
-        type: 'uint256'
+        "internalType": "uint256",
+        "name": "",
+        "type": "uint256"
       }
     ],
-    name: 'userToAudits',
-    outputs: [
+    "name": "userToAudits",
+    "outputs": [
       {
-        internalType: 'uint256',
-        name: '',
-        type: 'uint256'
+        "internalType": "uint256",
+        "name": "",
+        "type": "uint256"
       }
     ],
-    stateMutability: 'view',
-    type: 'function'
+    "stateMutability": "view",
+    "type": "function"
   }
 ]
