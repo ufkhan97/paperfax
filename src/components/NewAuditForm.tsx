8ddb3202e767bb02c3359c19e46a861b35d8f2e2
Lawrence Wu <llwu@llwu.me>
2021-10-10 12:37:53 -0700

    finished initial new audit component

--- src/components/NewAuditForm.tsx
+++ src/components/NewAuditForm.tsx
@@ -15,8 +15,20 @@ enum Verdict {
 
 const NewAuditForm: React.FC<NewAuditFormProps> = ({ paperfaxId }) => {
   const [verdict, setVerdict] = useState(Verdict.GREEN)
+  const [ipfsHash, setIpfsHash] = useState("0")
+  const [minorIssues, setMinorIssues] = useState(0)
+  const [majorIssues, setMajorIssues] = useState(0)
+  const [recommendations, setRecommendations] = useState(0)
   const handleVerdict = (e: any) => setVerdict(Number(e.target.value))
+  const handleIpfs = (e: React.ChangeEvent<HTMLInputElement>) => setIpfsHash(e.target.value)
+  const handleMinorIssues = (e: React.ChangeEvent<HTMLInputElement>) => setMinorIssues(Number(e.target.value) || 0)
+  const handleMajorIssues = (e: React.ChangeEvent<HTMLInputElement>) => setMajorIssues(Number(e.target.value) || 0)
+  const handleRecommendations = (e: React.ChangeEvent<HTMLInputElement>) => setRecommendations(Number(e.target.value) || 0)
   console.log(verdict)
+  console.log(ipfsHash)
+  console.log(minorIssues)
+  console.log(majorIssues)
+  console.log(recommendations)
 
   const { error, fetch, isFetching, isLoading } = useWeb3ExecuteFunction({
     abi: CONTRACT_ABI,
@@ -25,12 +37,17 @@ const NewAuditForm: React.FC<NewAuditFormProps> = ({ paperfaxId }) => {
     params: {
       _paperfaxId: paperfaxId,
       _verdict: `${verdict}`,
-      _ipfsHash: "0",
-      _numMinorIssues: "0",
-      _numMajorIssues: "0",
-      _numRecommendations: "0",
+      _ipfsHash: `${ipfsHash}`,
+      _numMinorIssues: `${minorIssues}`,
+      _numMajorIssues: `${majorIssues}`,
+      _numRecommendations: `${recommendations}`,
     }
   })
+
+  const handleSubmit = (e: React.FormEvent) => {
+    fetch()
+    e.preventDefault()
+  }
 
   return (
     <>
@@ -41,14 +58,27 @@ const NewAuditForm: React.FC<NewAuditFormProps> = ({ paperfaxId }) => {
           <pre>{JSON.stringify(error, null, 2)}</pre>
         </>
       )}
-      <div onChange={handleVerdict}>
-        <input type="radio" value="0" name="verdict" /> Green
-        <input type="radio" value="1" name="verdict" /> Yellow
-        <input type="radio" value="2" name="verdict" /> Red
-      </div>
-      <button onClick={() => fetch()} disabled={isFetching}>
-        Create
-      </button>
+      <form onSubmit={handleSubmit}>
+        <div onChange={handleVerdict}>
+          <input type="radio" value="0" name="verdict" checked={verdict == Verdict.GREEN} /> Green
+          <input type="radio" value="1" name="verdict" /> Yellow
+          <input type="radio" value="2" name="verdict" /> Red
+        </div>
+        <br />
+        <label htmlFor="ipfs">IPFS Hash:</label>
+        <input required id="ipfs" name="ipfs" onChange={handleIpfs} />
+        <br />
+        <label htmlFor="minor-issues">Number of minor issues:</label>
+        <input required type="number" min={0} id="minor-issues" name="minor-issues" onChange={handleMinorIssues} />
+        <br />
+        <label htmlFor="major-issues">Number of major issues:</label>
+        <input required type="number" min={0} id="major-issues" name="major-issues" onChange={handleMajorIssues} />
+        <br />
+        <label htmlFor="recommendations">Number of recommendations:</label>
+        <input required type="number" min={0} id="recommendations" name="recommendations" onChange={handleRecommendations} />
+        <br />
+        <button disabled={isFetching}>Create</button>
+      </form>
     </>
   )
 }
